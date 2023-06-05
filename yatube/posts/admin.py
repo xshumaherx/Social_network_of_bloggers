e7b7b4837f4444ca3c2e78f9ad2576556fd01146
Александр Вдовенко <Shumaher2014@mail.ru>
2023-06-05 22:27:44 +0400

    Сайт изменился на мифологию

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -5,7 +5,7 @@ from .models import Group, Post
 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'text', 'pub_date', 'author', 'group')
+    list_display = ('pk', 'titul', 'text', 'pub_date', 'author', 'group')
     list_editable = ('group',)
     search_fields = ('text',)
     list_filter = ('pub_date',)
